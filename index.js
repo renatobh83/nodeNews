76563f09e666548f0d3ecd406b24d68f969234a6
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-18 23:51:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium-min");
+const app = express();
+
+app.get('/screenshot', async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL não fornecida.' });
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+       args: ['--no-sandbox'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(
+        "https://github.com/Sparticuz/chromium/releases/download/v114.0.0/chromium-v114.0.0-pack.tar"
+      ),
+      headless: 'new',
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const screenshot = await page.screenshot({ fullPage: true });
+    await browser.close();
+    res.setHeader('Content-Type', 'image/png');
+    res.send(screenshot);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocorreu um erro ao capturar o screenshot.' });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Servidor iniciado. Acesse http://localhost:3000');
+});
